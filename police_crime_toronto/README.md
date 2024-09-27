5b6d213e1d00b73b97e692581107090d0452fa3f
wendyhuan <113368442+wendyhuan@users.noreply.github.com>
2024-09-27 17:00:20 -0400

    Add files via upload

---
+++ police_crime_toronto/README.md
@@ -0,0 +1,19 @@
+# A Decade of Crime in Toronto: Trends, Challenges and Responses (2014-2024)
+
+## Overview
+
+This repository provides an overview of the crime reported in Toronto between years 2014 and 2024. Using data of reported crime collected by opendatatoronto, we analyze the crime type and crime target under different districts of Toronto between 2014 and 2024. Our finding reveals that the crime types shift around time, with a marked rise in property-related crimes within ten years. These insights can help shape a comprehensive understanding of crime dynamics in Toronto during the 2014-2024 period and contribute to discussions on improving public safety and crime prevention strategies.
+
+## File Structure
+
+The repo is structured as:
+
+-   `data/raw_data` contains the raw data as obtained from opendatatoronto.
+-   `data/analysis_data` contains the cleaned dataset that was constructed.
+-   `other` contains relevant literature, details about LLM chat interactions, and sketches.
+-   `paper` contains the files used to generate the paper, including the Quarto document and reference bibliography file, as well as the PDF of the paper.
+-   `scripts` contains the R scripts used to simulate, download, clean, and test data.
+
+## Statement on LLM usage
+
+ChatGPT4o (4o-mini) was used as an LLM for this project. Debugging, formatting, and commenting were done using LLM. All chat history for the LLM usage can be found in the `inputs/llm/usage.txt` folder
---
+++ police_crime_toronto/scripts/03-test_data.R
@@ -0,0 +1,60 @@
+#### Preamble ####
+# Purpose: Test if the simulated data has NA or other unneeded data
+# Author: Yanfei Huang
+# Date: 25 September 2024
+# Contact: yanfei.huang@mail.utoronto.ca
+# License: University of Toronto
+# Pre-requisites: simulated data
+# Any other information needed? None
+
+
+#### Workspace setup ####
+library(tidyverse)
+
+#### Test data ####
+simulated_data <- read_csv("data/raw_data/simulated_data.csv")
+
+# Check year range
+year_range <- simulated_data$reported_y
+test_year_range <- all(year_range >= 2014 & year_range <= 2024)
+test_year_range
+
+# Test if all crime types are within the defined set
+test_crime_type <- all(simulated_data$crime_type %in% 
+                         c("Auto Theft","SEXUAL Violation", "Fraud", "Attempt Murder")
+)
+
+test_crime_type
+
+# Test if all divisions are within the defined set
+test_divisions <- all(simulated_data$division 
+                      %in% c("D11", "D12", "D13", "D14", "D22", "D23", "D31", 
+                             "D32", "D33", "D41", "D42", "D43", "D51", "D52", 
+                             "D53", "D55"))
+
+test_divisions
+
+# Test if all crime against are within the defined set
+test_crime_towards <- all(simulated_data$crime_towards 
+                          %in% c("Crimes Against Property", "Crimes Against the Person"))
+test_crime_towards
+
+# Test if all cleared values are only 0 or 1
+test_cleared_values <- all(simulated_data$cleared %in% c(0, 1))
+
+test_cleared_values
+
+# Test data type for simulated_data
+class(simulated_data$reported_y) == "numeric"
+class(simulated_data$division) == "character"
+class(simulated_data$crime_towards) == "character"
+class(simulated_data$crime_type) == "character"
+class(simulated_data$cleared) == "numeric"
+
+# Test for NAs
+all(is.na(simulated_data$reported_y))
+all(is.na(simulated_data$division))
+all(is.na(simulated_data$crime_towards))
+all(is.na(simulated_data$cleared))
+all(is.na(simulated_data$crime_type))
+
